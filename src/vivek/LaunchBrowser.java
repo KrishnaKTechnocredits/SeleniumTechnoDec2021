db232e3c26e8e2afc0e6e41212aab6afd17b8122
krishnaktechnocredits <harshhpatel07@gmail.com>
2022-02-14 10:18:24 +0530

    project upload

---
+++ src/vivek/LaunchBrowser.java
@@ -0,0 +1,18 @@
+package vivek;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver_98Version.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		System.out.println("Maximized browser");
+		driver.get("http://automationbykrishna.com/");
+		System.out.println("Browser launched");
+		driver.close();
+		System.out.println("Browser Closed");
+	}
+}
