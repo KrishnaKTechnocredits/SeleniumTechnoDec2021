b9f3a07be8bc2ae248f954c2627041bcdb513ef2
Vrushali <vrushalip90@gmail.com>
2022-02-25 23:52:11 +0530

    ElemntClickedInterceptedException_program_updated

--- src/vrushali/A2_ElementClickInterceptedException.java
+++ src/vrushali/A2_ElementClickInterceptedException.java
@@ -11,12 +11,16 @@ public class A2_ElementClickInterceptedException {
 
 	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver_98Version.exe");
 	WebDriver driver = new ChromeDriver();
-	driver.manage().window().maximize();
-	System.out.println("Maximized browser");
-	driver.get("file:///C:/Users/Sagar/Downloads/htmlForms/Harsh_Enable_&_Disable.html");
+	/*driver.manage().window().maximize();
+	System.out.println("Maximized browser");*/
+	driver.get("http://automationbykrishna.com/");
 	System.out.println("Browser launched");
-	driver.findElement(By.id("myText")).sendKeys("Vrushali"); // it will generate A2_ElementClickInterceptedException
-	
+	driver.findElement(By.id("basicelements")).click(); 
+	Thread.sleep(2000);
+	driver.findElement(By.xpath("//button[@id='javascriptAlert']")).click();
+	Alert alert = driver.switchTo().alert();
+	System.out.println("Switch to alert");
+	alert.accept();
 	driver.close();
 	System.out.println("Browser Closed");
 	
