1478a3de1440300a0c9f494330c60d4d53362896
Yogesh Nagar <yogesh61189@gmail.com>
2022-03-09 07:03:39 +0530

    YogeshNagarAssignment13

--- src/yogeshNagar/A13_DataProviderExcel.java
+++ src/yogeshNagar/A13_DataProviderExcel.java
@@ -17,54 +17,53 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class A13_DataProviderExcel {
-	
-	@Test(dataProvider="testData")
-	public void readExcelData(String UserName, String PassWord, String Message) {
-		WebDriver driver = BaseClass.launchBrowser("http://automationbykrishna.com/");
-		driver.findElement(By.id("registration2")).click();
-		WebDriverWait wait = new WebDriverWait(driver,10);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("unameSignin")));
-		WebElement userName = driver.findElement(By.id("unameSignin"));
-		WebElement passWord = driver.findElement(By.id("pwdSignin"));
-		WebElement subBtn = driver.findElement(By.id("btnsubmitdetails"));
-		userName.click();
-		userName.sendKeys(UserName);
-		passWord.click();
-		passWord.sendKeys(PassWord);
-		subBtn.click();
-		Alert alert = driver.switchTo().alert();
-		if(Message.equals(alert.getText()))
-			System.out.println("Pass");
-		else
-			System.out.println("Fail");	
-		
-		alert.accept();
-		driver.quit();
-	}
-	
-	@DataProvider(name="testData")
-	public Object[][] getData() throws IOException{
-		File file = new File("./src/yogeshNagar/resource/testData.xlsx");
-		FileInputStream input = new FileInputStream(file);
 
-		Workbook workbook = new XSSFWorkbook(input);
-		Sheet sheet = workbook.getSheet("Sheet1");
+    @Test(dataProvider = "testData")
+    public void readExcelData(String UserName, String PassWord, String Message) {
+        WebDriver driver = BaseClass.launchBrowser("http://automationbykrishna.com/");
+        driver.findElement(By.id("registration2")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("unameSignin")));
+        WebElement userName = driver.findElement(By.id("unameSignin"));
+        WebElement passWord = driver.findElement(By.id("pwdSignin"));
+        WebElement subBtn = driver.findElement(By.id("btnsubmitdetails"));
+        userName.click();
+        userName.sendKeys(UserName);
+        passWord.click();
+        passWord.sendKeys(PassWord);
+        subBtn.click();
+        Alert alert = driver.switchTo().alert();
+        if (Message.equals(alert.getText()))
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
 
-		int totalRow = sheet.getLastRowNum() + 1;
-		int totalCol = sheet.getRow(0).getLastCellNum();
+        alert.accept();
+        driver.quit();
+    }
 
-		System.out.println("Row : " + totalRow);
-		System.out.println("Col : " + totalCol);
+    @DataProvider(name = "testData")
+    public Object[][] getData() throws IOException {
+        File file = new File("./src/yogeshNagar/resource/testData.xlsx");
+        FileInputStream input = new FileInputStream(file);
 
-		Object[][] data = new Object[totalRow][totalCol];
+        Workbook workbook = new XSSFWorkbook(input);
+        Sheet sheet = workbook.getSheet("Sheet1");
 
-		for (int rowCount = 0; rowCount < totalRow; rowCount++) {
-			for (int colCnt = 0; colCnt < totalCol; colCnt++) {
-				String cellValue = sheet.getRow(rowCount).getCell(colCnt).getStringCellValue();
-				data[rowCount][colCnt] = cellValue;
-			}
-		}
-		return data;
-	}
+        int totalRow = sheet.getLastRowNum() + 1;
+        int totalCol = sheet.getRow(0).getLastCellNum();
 
+        System.out.println("Row : " + totalRow);
+        System.out.println("Col : " + totalCol);
+
+        Object[][] data = new Object[totalRow][totalCol];
+
+        for (int rowCount = 0; rowCount < totalRow; rowCount++) {
+            for (int colCnt = 0; colCnt < totalCol; colCnt++) {
+                String cellValue = sheet.getRow(rowCount).getCell(colCnt).getStringCellValue();
+                data[rowCount][colCnt] = cellValue;
+            }
+        }
+        return data;
+    }
 }
